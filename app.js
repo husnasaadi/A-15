0dfc89ee43ffee2cdcabd2a6ad97e585cafcef9e
sadia saadi <saadisadia204@gmail.com>
2024-02-28 13:13:44 +0500

    about js

---
+++ app.js
@@ -0,0 +1,104 @@
+// chap 4
+// // q ;1 
+// var x=1, y=2 ,z=3
+// Q2
+// Legal Variable Names:
+
+// myVariable
+// _myVariable
+// $myVariable
+// my_variable
+// myVariable123
+
+// Illegal Variable Names:
+
+// 123myVariable 
+// my Variable 
+// my-Variable 
+// var if
+// myVariable!
+
+// q;3
+// document.write(“Rules for naming JS variables”)
+// letter 
+// digits 
+// dollar sign 
+// under scroes 
+
+// // d
+// case camel 
+// // e
+// js funtions
+
+// cha 5
+// q;1
+// var number=3
+// var add=number+5
+// document.write(add)
+
+// Q;2
+// multiply
+// var number=2
+// var multiply=number*4
+// document.write(multiply)
+// // division
+// var number=16%2
+// document.write(number)
+
+// q;5
+
+// var table= +prompt("enter yur table");
+// document.write(
+//  table + " * 1 = " + table * 1 + "<br> "+
+//  table + " * 2 = " + table * 2 + "<br> "+
+//  table + " * 3 = " + table * 3 + "<br>"+
+//  table + " * 4 = " + table * 4 + "<br>"+
+//  table + " * 5 = " + table * 5 + "<br>"+
+//  table + " * 6 = " + table * 6 + "<br>"+
+//  table + " * 7 = " + table * 7 + "<br>"+
+//  table + " * 8 = " + table * 8 + "<br>"+
+//  table + " * 9 = " + table * 9 + "<br>"+
+//  table + " * 10 = " + table * 10 + "<br>"
+// );
+// // q;3
+// // a
+// var my_variable=12;
+// // b,c
+// var myVariable=12+my_variable;
+// // d
+// document.write(myVariable)
+// // e
+// var incr=4+myVariable;
+// document.write(incr);
+
+
+// prompt
+// // q;1
+// var firstName=prompt("enter your first name");
+// // q;2
+// var country= prompt("countrty ", "china");
+// // q;3
+// var yourName = prompt("Enter Your Name");
+// // q;4
+// var msg= prompt("enter your msg","tell fav color");
+// // q;5
+// var message=husna ;
+// var mes=saadi ;
+// var defaultresponse=prompt(message, mes);
+
+
+// concentating
+// q;1
+// var num ="2"+"2";
+// alert(num)
+// q;2
+// var message="hello"+ "dolly";
+// alert('"' + message + '"');
+// q;3
+// alert("33" + 3);
+// q;4
+var pak=pakistan ;
+var zin=zindabad;
+var pk=pak+zin
+alert(pk)
+
